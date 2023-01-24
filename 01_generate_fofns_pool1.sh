31c98cd3a9c7da23859d6242fe4a7fd4682c12c7
blumeria <simone.oberhaensli@gmail.com>
2023-01-24 14:03:10 +0100

    Update 01_generate_fofns_pool1.sh

--- 01_generate_fofns_pool1.sh
+++ 01_generate_fofns_pool1.sh
@@ -2,9 +2,9 @@
 
 # the pacbio SMRTtools assembly pipeline takes as input bam format
 
-export listdir='/data/projects/p446_Dialact_Phoenix/2_analyses/B_pacbio/202301_assemblies/1_rawdata/fofn_lists_pool1'
-export SMRT1='/data/projects/p446_Dialact_Phoenix/2_analyses/B_pacbio/202301_assemblies/1_rawdata/r64293e_20221223_103139_1_A01_pool1/demux_bam'
-export SMRT2='/data/projects/p446_Dialact_Phoenix/2_analyses/B_pacbio/202301_assemblies/1_rawdata/r64293e_20221228_134539_1_A01_pool1/demux_bam'
+export listdir='/1_rawdata/fofn_lists_pool1'
+export SMRT1='/1_rawdata/r64293e_20221223_103139_1_A01_pool1/demux_bam'
+export SMRT2='/1_rawdata/r64293e_20221228_134539_1_A01_pool1/demux_bam'
 
 declare -a barcodes
 readarray -t barcodes < sample_barcode_map_pool1.csv
